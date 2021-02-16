667ffc6d2fc027c2b5f7b3334b7aec14a8c62f77
Arjun99ab <samavedamarjun@gmail.com>
2021-02-16 13:54:48 -0500

    wqrqwr
    adad

--- spamBot/friend.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-friend = pyttsx3.init()
-voices = friend.getProperty('voices')
-for voice in voices:
-    print(voice, voice.id)
-    friend.setProperty('voice', voice.id)
-    friend.say("Hello World!")
-    friend.runAndWait()
-    friend.stop()
--- spamBot/keyboardTest.py
+++
@@ -1,8 +0,0 @@
-import keyboard, pyautogui
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('5'):  # if key '5' is pressed
-            print('You pressed a key!')
-            break  # finishing the loop5
-    except:
-        break  # if user pressed a key other than the given key the loop will break
